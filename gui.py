f5f9d14c86d9f9997d3b70cbea313e788302fe96
aaresecheona <aa324830@ucf.edu>
2023-09-26 16:24:16 -0400

    update

--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ class MinesweeperGUI:
         self.root.title("Minesweeper")
 
         self.root.geometry("700x500")
+        self.root.configure(background='#000000')
 
         self.label = tk.Label(self.root, text='Minesweeper', font=("Ariel", 22))
         self.label.pack(padx=10, pady=10)
@@ -22,12 +23,11 @@ class MinesweeperGUI:
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
     
-    def createButtonColumns(self, numColumns, numRows):
-        t = 1
+    def createButtonGrid(self, numColumns, numRows):
         for colNum in range(1,numColumns+1):
             self.buttonFrame.columnconfigure(colNum, weight=1)
             for rowNum in range(1, numRows+1):
-                self.btnDic["btn{0}".format(str(colNum)+'.'+str(rowNum))] = tk.Button(self.buttonFrame, text = str(t), font=('Ariel', 15))
+                self.btnDic["btn{0}".format(str(colNum)+'.'+str(rowNum))] = tk.Button(self.buttonFrame, font=('Ariel', 15), height=1, width=3)
                 self.btnDic["btn{0}".format(str(colNum)+'.'+str(rowNum))].grid(row=rowNum, column=colNum, sticky='news')
                 t+=1
         self.buttonFrame.pack()
@@ -42,5 +42,5 @@ class MinesweeperGUI:
 
 
 game = MinesweeperGUI()
-game.createButtonColumns(18,14)
+game.createButtonGrid(5,5)
 game.start()
