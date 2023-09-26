6a73f891f4802336babb88025768248750d3fdd4
aaresecheona <aa324830@ucf.edu>
2023-09-26 14:34:29 -0400

    can create a specified number of rows and columns

--- gui.py
+++ gui.py
@@ -15,18 +15,32 @@ class MinesweeperGUI:
         self.label = tk.Label(self.root, text='Minesweeper', font=("Ariel", 22))
         self.label.pack(padx=10, pady=10)
 
+        self.buttonFrame = tk.Frame(self.root)
 
+        self.btnDic = {}
 
-        
-        
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
-        self.root.mainloop()
+
     
+    def createButtonColumns(self, numColumns, numRows):
+        t = 1
+        for colNum in range(1,numColumns+1):
+            self.buttonFrame.columnconfigure(colNum, weight=1)
+            for rowNum in range(1, numRows+1):
+                self.btnDic["btn{0}".format(str(colNum)+'.'+str(rowNum))] = tk.Button(self.buttonFrame, text = str(t), font=('Ariel', 15))
+                self.btnDic["btn{0}".format(str(colNum)+'.'+str(rowNum))].grid(row=rowNum, column=colNum, sticky='news')
+                t+=1
+        self.buttonFrame.pack()
 
 
     def on_close(self):
         if messagebox.askyesno(title="Quit?", message='Are you sure you want to quit?'):
             self.root.destroy()
 
+    def start(self):
+        self.root.mainloop()
 
-MinesweeperGUI()
+
+game = MinesweeperGUI()
+game.createButtonColumns(18,14)
+game.start()
